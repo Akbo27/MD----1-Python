718cf47ef240d1ebc575806089b1396ff21df5d5
Akbo27 <aungkaungbo.edu@gmail.com>
2024-09-13 14:04:14 +0700

    Work1_Turtle

---
+++ work1_turtle.py
@@ -0,0 +1,180 @@
+import turtle
+
+turtle.speed(51)
+turtle.shape('turtle')
+ninja = turtle.clone()
+
+turtle.color('white' , 'green')
+turtle.begin_fill()
+
+turtle.forward(300)
+turtle.left(90)
+turtle.forward (150)
+turtle.left(90)
+turtle.forward (300)
+turtle.left(90)
+turtle.forward (150)
+turtle.color()
+
+turtle.end_fill()
+
+turtle.home()
+
+turtle.forward (150)
+turtle.left(90)
+turtle.forward (150)
+turtle.backward (75)
+#turtle.circle(20)
+turtle.forward (75)
+turtle.left(90)
+turtle.forward (150)
+
+turtle.left(90)
+turtle.forward (50)
+turtle.left(90)
+turtle.forward (25)
+turtle.left(-90)
+turtle.forward (50)
+turtle.left(-90)
+turtle.forward (25)
+
+turtle.left(90)
+turtle.forward (50)
+turtle.right(-90)
+turtle.forward (300)
+turtle.left(90)
+turtle.forward (50)
+turtle.left(90)
+turtle.forward (25)
+turtle.left(-90)
+turtle.forward (50)
+turtle.left(-90)
+turtle.forward (25)
+
+turtle.left(90)
+turtle.forward (15)
+turtle.left(90)
+turtle.forward (45)
+turtle.left(90)
+turtle.forward (80)
+turtle.right(-90)
+turtle.forward (45)
+
+turtle.left(-90)
+turtle.forward (35)
+turtle.left(-90)
+turtle.forward (300)
+
+turtle.right(90)
+turtle.forward (35)
+turtle.left(-90)
+turtle.forward (45)
+turtle.left(90)
+turtle.forward (80)
+turtle.right(-90)
+turtle.forward (45)
+
+turtle.right(180)
+turtle.forward (35)
+turtle.left(-90)
+turtle.penup()
+turtle.forward (22.5)
+turtle.forward (40)
+turtle.pendown()
+turtle.left(90)
+turtle.penup()
+turtle.forward (15)
+turtle.pendown()
+turtle.circle(20 , 180)
+
+turtle.penup()
+turtle.home()
+turtle.forward (300)
+turtle.left(90)
+turtle.forward (75)
+turtle.left(90)
+turtle.forward (50)
+turtle.left(-90)
+turtle.forward (22.5)
+turtle.left(90)
+turtle.pendown()
+turtle.circle(20 , 180)
+turtle.hideturtle()
+ninja.hideturtle()
+
+
+ninja.color('white' , 'green')
+ninja.begin_fill()
+
+
+ninja.home()
+ninja.forward (150)
+ninja.left(90)
+ninja.forward (95)
+ninja.left(90)
+
+ninja.circle(20)
+
+turtle.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#turtle.forward (-25)
+#turtle.left(180)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#turtle.color("green")
+#turtle.circle (100 , 180)
+
+turtle.mainloop()
